790b1207c9d12f68f9b4fa20830f1478ab08b78c
doubleUTF <doubleUTF@users.noreply.github.com>
2018-10-21 16:21:18 -0700

    Removed unnecessary lines
    Since no unit tests are required, might as well remove all unnecessary imports.  There is a line that imports the stockhandler which doesn't even exists in the current repo.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,11 +6,6 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chai = require('chai');
-var StockHandler = require('../controllers/stockHandler.js');
-
-var stockPrices = new StockHandler();
-
 suite('Unit Tests', function(){
 
 //none requiered
