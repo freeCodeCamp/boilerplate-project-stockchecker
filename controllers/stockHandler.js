0e08a60f09fbf127e63bff667b93f2d9bf04781a
John Pham <mr.john.pham@gmail.com>
2020-06-21 15:48:38 -0400

    Create options for stockHandler

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -1,7 +1,19 @@
 const https = require('https');
 
 function stockHandler (req, res) {
-    console.log(`req is ${JSON.stringify(req.query)}`);
+    const stock = req.query.stock;
+
+    let options = {
+        hostname: 'https://repeated-alpaca.glitch.me/v1/',
+        port: 443,
+        path: '/v1/stock/' + stock + '/quote',
+        method: 'GET'
+    }
+
+    console.log(`req.query.stock is ${JSON.stringify(req.query.stock)}`);
+
+
+
 }
 
 module.exports = stockHandler;
