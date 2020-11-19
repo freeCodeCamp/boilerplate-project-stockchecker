b765c3e5dede89807ed9f877dcac3731008ad767
nhcarrigan <nhcarrigan@gmail.com>
2020-11-19 08:49:42 -0800

    Move tests to /learn
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,28 +6,5 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    
-    suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
 
-      });
-      
-      test('1 stock with like', function(done) {
-        
-      });
-      
-      test('1 stock with like again (ensure likes are not double counted)', function(done) {
-        
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
-    });
 });
