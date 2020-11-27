72c6850551e1a16ebff2776b92fec59679e63150
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2020-11-27 09:51:59 -0800

    Update repo, remove extraneous files (#13)
    * Update repo, remove extraneous files
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>
    
    * Remove comments
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>
    
    * Move tests to /learn
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>
    
    * Add dotenv, update package-lock
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>
    
    * Change price from str to float
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>
    
    * Remove mongodb
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,4 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
-
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 'use strict';
+require('dotenv').config();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
-
-var app = express();
+const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
