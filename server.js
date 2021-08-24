d9e1b55209ddce0662c21b72e918bcaed3442829
moT01 <20648924+moT01@users.noreply.github.com>
2021-08-24 10:35:36 -0500

    fix: log listening port

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
