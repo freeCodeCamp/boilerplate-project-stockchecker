86f65b985fc772580177e4bc5f7fe19c2fae310a
KUSH328 <67806118+KUSH328@users.noreply.github.com>
2022-08-18 12:53:10 +0300

    2_functional-tests.js
    This is the solution as per freecode camp instructions

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,127 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', async function() {
+
+  await suite('GET /api/stock-prices', async () =>{
+
+    await test('Viewing one stock', done =>{
+      chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'GOOG'})
+        .end((err, res) => {
+
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.hasAllKeys(res.body.stockData, [
+            'price',
+            'stock',
+            'likes']);
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
+          
+          done();
+        });
+    });
+
+    await test('Viewing one stock and liking it', done =>{
+      chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'GOOG', like: true })
+        .end((err, res) => {
+
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.hasAllKeys(res.body.stockData, [
+            'price',
+            'stock',
+            'likes']);
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
+          
+          done();
+        });
+    });
+
+    await test('Viewing one stock and liking it again', done =>{
+      chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'GOOG', like: true })
+        .end((err, res) => {
+
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.hasAllKeys(res.body.stockData, [
+            'price',
+            'stock',
+            'likes']);
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
+          
+          done();
+        });
+    });
+
+    await test('Viewing two stocks', done =>{
+      chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: ['GOOG','MSFT'] })
+        .end((err, res) => {
+
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.isArray(res.body.stockData);
+          assert.hasAllKeys(res.body.stockData[0], [
+            'price',
+            'stock',
+            'rel_likes']);
+            assert.hasAllKeys(res.body.stockData[1], [
+              'price',
+              'stock',
+              'rel_likes']);
+          assert.equal(res.body.stockData[0].stock, 'GOOG');
+          assert.equal(res.body.stockData[1].stock, 'MSFT');
+          assert.isNumber(res.body.stockData[0].price);
+          assert.isNumber(res.body.stockData[1].price);
+          assert.isNumber(res.body.stockData[0].rel_likes);
+          assert.isNumber(res.body.stockData[1].rel_likes);
+          
+          done();
+        });
+    });
+
+    await test('Viewing two stocks and liking them', done =>{
+      chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: ['GOOG','MSFT'], like: true })
+        .end((err, res) => {
+
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.isArray(res.body.stockData);
+          assert.hasAllKeys(res.body.stockData[0], [
+            'price',
+            'stock',
+            'rel_likes']);
+            assert.hasAllKeys(res.body.stockData[1], [
+              'price',
+              'stock',
+              'rel_likes']);
+          assert.equal(res.body.stockData[0].stock, 'GOOG');
+          assert.equal(res.body.stockData[1].stock, 'MSFT');
+          assert.isNumber(res.body.stockData[0].price);
+          assert.isNumber(res.body.stockData[1].price);
+          assert.isNumber(res.body.stockData[0].rel_likes);
+          assert.isNumber(res.body.stockData[1].rel_likes);
+          
+          done();
+        });
+    });
+
+
+  });
 
 });
