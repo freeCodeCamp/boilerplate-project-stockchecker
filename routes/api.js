7471e83508415df4205cae10376a61b24b35fcb1
ndegwa Wa Ndungu <ndegwaduncan193@gmail.com>
2022-10-10 05:54:05 +0000

    updated xss settings in server.js

--- routes/api.js
+++ routes/api.js
@@ -1,208 +1,234 @@
-'use strict';
-const axios = require('axios');
-const mongoose = require('mongoose');
-const anonymize = require('ip-anonymize');
+"use strict";
+const axios = require("axios");
+const mongoose = require("mongoose");
+const anonymize = require("ip-anonymize");
 
 const stockDetails = new mongoose.Schema({
   stock: String,
   price: Number,
   likes: Number,
-  reqIP: Array
+  reqIP: Array,
 });
 
-const StockDetails = new mongoose.model('StockDetails', stockDetails);
+const StockDetails = new mongoose.model("StockDetails", stockDetails);
 
 module.exports = function (app) {
+  app.route("/api/stock-prices").get(function (req, res) {
+    const { stock, like } = req.query;
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      let stockData
-      const {stock, like} = req.query
-      
-      let  reqIP = anonymize(req.ip, 16, 16)
+    let reqIP = anonymize(req.ip, 16, 16);
 
-      console.log('stock:', stock, 'like:', like, 'reqIP:', reqIP, '(1)');
-      
-      async function updateOrCreateNewStock(stock, like, reqIP){
-        // console.log('checking for stock details ....')
-        // console.log('stock:', stock, 'like:', like, 'reqIP:', reqIP, '(2)');
-        const existingStockDetails = await StockDetails.findOne({stock: stock})
-        
-        if(!existingStockDetails){
-          // console.log('There is no similar existingStock');
-            const newStockDetails = await StockDetails.create({
-                stock: '',
-                price: '',
-                likes: '',
-                reqIP: []
-            });
-          
-            await newStockDetails.save();
+    console.log("stock:", stock, "like:", like, "reqIP:", reqIP, "(1)");
 
-            let price = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`).then(response => { return response.data.close }).catch(error => {
-              // console.log('error:', error)  
-              return error
-            } );
-          // console.log("newStockDetails' price:", price)
-            
-            if(price != null){
-              newStockDetails.stock = stock
-              newStockDetails.price = price
-                if(like === 'true'){
-                  newStockDetails.likes = 1 
-                  newStockDetails.reqIP = newStockDetails.reqIP.push(reqIP);
-                };
-            }else{
-                if(like === 'true'){
-                  newStockDetails.likes = 1 
-                  newStockDetails.reqIP = newStockDetails.reqIP.push(reqIP);
-                };
-            }
+    async function updateOrCreateNewStock(stock, like, reqIP) {
+      // console.log('checking for stock details ....')
+      console.log("stock:", stock, "like:", like, "reqIP:", reqIP, "(2)");
+      const existingStockDetails = await StockDetails.findOne({ stock: stock });
 
+      if (!existingStockDetails) {
+        // console.log('There is no similar existingStock');
+        const newStockDetails = await StockDetails.create({
+          stock: "",
+          price: "",
+          likes: "",
+          reqIP: [],
+        });
 
-            await newStockDetails.save();
+        await newStockDetails.save();
+
+        let price = await axios
+          .get(
+            `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+          )
+          .then((response) => {
+            return response.data.close;
+          })
+          .catch((error) => {
+            // console.log('error:', error)
+            return error;
+          });
+        // console.log("newStockDetails' price:", price)
+
+        if (price != null) {
+          newStockDetails.stock = stock;
+          newStockDetails.price = price;
+          if (like === "true") {
+            newStockDetails.likes = 1;
+            newStockDetails.reqIP = newStockDetails.reqIP.push(reqIP);
+          } else {
+            newStockDetails.likes = 0;
+          }
+        } else {
+          if (like === "true") {
+            newStockDetails.likes = 1;
+            newStockDetails.reqIP = newStockDetails.reqIP.push(reqIP);
+          } else {
+            newStockDetails.likes = 0;
+          }
+        }
+
+        await newStockDetails.save();
+        console.log("newStockDetails: ", newStockDetails);
+        return {
+          stock: newStockDetails.stock,
+          price: newStockDetails.price,
+          likes: newStockDetails.likes,
+        };
+      } else {
+        // console.log('There is similar existingStock');
+        // console.log('1. existingStockDetails before:', existingStockDetails)
+
+        let price = await axios
+          .get(
+            `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+          )
+          .then((response) => {
+            return response.data.close;
+          })
+          .catch((error) => {
+            // console.log('price error:', error)
+            return error;
+          });
+        existingStockDetails.price === price
+          ? (existingStockDetails.price = price)
+          : (existingStockDetails.price = existingStockDetails.price);
+
+        await existingStockDetails.save();
+
+        if (
+          existingStockDetails.reqIP.includes(reqIP) === false &&
+          like === "true"
+        ) {
+          existingStockDetails.likes = existingStockDetails.likes += 1;
+          existingStockDetails.reqIP = existingStockDetails.reqIP.push(reqIP);
+
+          console.log("existingStockDetails 1. :", existingStockDetails);
+          return {
+            stock: existingStockDetails.stock,
+            price: existingStockDetails.price,
+            likes: existingStockDetails.likes,
+          };
+        } else {
+          console.log("existingStockDetails 2. :", existingStockDetails);
 
           return {
-              stock: newStockDetails.stock,
-              price: newStockDetails.price,
-              likes: newStockDetails.likes
+            stock: existingStockDetails.stock,
+            price: existingStockDetails.price,
+            likes: existingStockDetails.likes,
           };
-        }else{
-            // console.log('There is similar existingStock');
-            // console.log('1. existingStockDetails before:', existingStockDetails)
-          
-              let price = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`).then(response => { return response.data.close }).catch(error => {
-                // console.log('price error:', error)
-                return error
-        });
-            existingStockDetails.price === price ? existingStockDetails.price = price : existingStockDetails.price = existingStockDetails.price;
-
-            await existingStockDetails.save();
-
-              if(existingStockDetails.reqIP.includes(reqIP) === false && like === 'true') {
-                      existingStockDetails.likes = existingStockDetails.likes += 1;
-                      existingStockDetails.reqIP = existingStockDetails.reqIP.push(reqIP);
-        
-                // console.log('1. existingStockDetails after:', existingStockDetails);
-                      return {
-                        stock: existingStockDetails.stock,
-                        price: existingStockDetails.price,
-                        likes: existingStockDetails.likes
-                      };
-                  }else{
-               // console.log('2. existingStockDetails after:', existingStockDetails);
-
-                return {
-                  stock: existingStockDetails.stock,
-                  price: existingStockDetails.price,
-                  likes: existingStockDetails.likes
-                };
-              }
-        };
-
-      };
-
-      async function collectstocks(stock){
-        let stocks = []
-        if(Array.isArray(stock)){
-          for(let i=0; i < stock.length; i++){
-            stocks.push(await stock[i]) 
-          }
-        }else{
-          stocks.push(await stock) 
         }
-        return stocks;
       }
-      
-    async function getStockData(stock, like, reqIP) {
-      let stocks = await collectstocks(stock)
-        if(like==='true'){
-          if(stocks.length > 1){
-            
-            let stockData1 = await updateOrCreateNewStock(stocks[0], like, reqIP)
-            
-            let stockData2 = await updateOrCreateNewStock(stocks[1], like, reqIP);
-
-    Promise.all([stockData1, stockData2]).then(values =>{          
-          let data = [];  
-                                                                                     values.map(value => {data.push(value)});
-              res.json({stockData: [
-                {
-                 stock: data[0].stock,
-                 price: data[0].price,
-                 rel_likes: data[0].likes -  data[1].likes
-                },
-                {
-                 stock: data[1].stock,
-                 price: data[1].price,
-                 rel_likes: data[1].likes -  data[0].likes
-                }
-              ]})
-            });
-            
-          }else{
-            let stockData = updateOrCreateNewStock(stocks[0], like, reqIP);
-             stockData.then(data => {
-
-               if(data.stock && data.price){
-                  res.json({stockData: {
-                    stock: data.stock,
-                    price: data.price,
-                    likes: data.likes
-                  }
-                 })
-              }else{
-                  res.json({stockData: {
-                    likes: data.likes
-                  }})
-              }
-
-            })
-          }
-        }else if(like==='false'){
-           if(stocks.length > 1){
-            
-            let stockData1 = await updateOrCreateNewStock(stocks[0], like, reqIP);            
-            let stockData2 = await updateOrCreateNewStock(stocks[1], like, reqIP);
-
-            Promise.all([stockData1, stockData2]).then(values =>{          
-              let data = []  
-                                                                                         values.map(value => {data.push(value)})
-                res.json({stockData: [
-                {
-                 stock: data[0].stock,
-                 price: data[0].price,
-                 rel_likes: data[0].likes -  data[1].likes
-                },
-                {
-                 stock: data[1].stock,
-                 price: data[1].price,
-                 rel_likes: data[1].likes -  data[0].likes
-                }
-              ]})
-            });
-            
-          }else{
-            await updateOrCreateNewStock (stocks[0], like, reqIP)
-              .then(data => {
-                if(data.stock && data.price){
-                    res.json({stockData: {
-                      stock: data.stock,
-                      price: data.price,
-                      likes: data.likes
-                    }
-                   })
-                }else{
-                    res.json({stockData: {
-                      likes: data.likes
-                    }})
-                }
-            })    
-          };
-        };
     }
 
-    getStockData(stock, like, reqIP);
-    
-    });
+    async function collectstocks(stock) {
+      let stocks = [];
+      if (Array.isArray(stock)) {
+        for (let i = 0; i < stock.length; i++) {
+          stocks.push(await stock[i]);
+        }
+      } else {
+        stocks.push(await stock);
+      }
+      return stocks;
+    }
+
+    async function getStockData(stock, like, reqIP) {
+      let stocks = await collectstocks(stock);
+      if (like === "true") {
+        if (stocks.length > 1) {
+          let stockData1 = await updateOrCreateNewStock(stocks[0], like, reqIP);
+
+          let stockData2 = await updateOrCreateNewStock(stocks[1], like, reqIP);
+
+          Promise.all([stockData1, stockData2]).then((values) => {
+            let data = [];
+            values.map((value) => {
+              data.push(value);
+            });
+            res.json({
+              stockData: [
+                {
+                  stock: data[0].stock,
+                  price: data[0].price,
+                  rel_likes: data[0].likes - data[1].likes,
+                },
+                {
+                  stock: data[1].stock,
+                  price: data[1].price,
+                  rel_likes: data[1].likes - data[0].likes,
+                },
+              ],
+            });
+          });
+        } else {
+          let stockData = updateOrCreateNewStock(stocks[0], like, reqIP);
+          stockData.then((data) => {
+            if (data.stock && data.price) {
+              res.json({
+                stockData: {
+                  stock: data.stock,
+                  price: data.price,
+                  likes: data.likes,
+                },
+              });
+            } else {
+              res.json({
+                stockData: {
+                  likes: data.likes,
+                },
+              });
+            }
+          });
+        }
+      } else if (like === "false") {
+        if (stocks.length > 1) {
+          let stockData1 = await updateOrCreateNewStock(stocks[0], like, reqIP);
+          let stockData2 = await updateOrCreateNewStock(stocks[1], like, reqIP);
+
+          Promise.all([stockData1, stockData2]).then((values) => {
+            let data = [];
+            values.map((value) => {
+              data.push(value);
+            });
+            res.json({
+              stockData: [
+                {
+                  stock: data[0].stock,
+                  price: data[0].price,
+                  rel_likes: data[0].likes - data[1].likes,
+                },
+                {
+                  stock: data[1].stock,
+                  price: data[1].price,
+                  rel_likes: data[1].likes - data[0].likes,
+                },
+              ],
+            });
+          });
+        } else {
+          await updateOrCreateNewStock(stocks[0], like, reqIP).then((data) => {
+            if (data.stock && data.price) {
+              res.json({
+                stockData: {
+                  stock: data.stock,
+                  price: data.price,
+                  likes: data.likes,
+                },
+              });
+            } else {
+              res.json({
+                stockData: {
+                  likes: data.likes,
+                },
+              });
+            }
+          });
+        }
+      }
+    }
+
+    if (stock != null) {
+      getStockData(stock, like, reqIP);
+    }
+  });
 };
--- server.js
+++ server.js
@@ -1,66 +1,66 @@
-'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
 const app = express();
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; script-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; style-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; connect-src 'https://replit.com/@duncanndegwa/boilerplate-project-stockchecker#public/script.js';"
+    "Content-Security-Policy",
+    // "default-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; script-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; style-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; connect-src 'https://replit.com/@duncanndegwa/boilerplate-project-stockchecker#public/script.js';"
+    "script-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; style-src 'self' https://boilerplate-project-stockchecker.duncanndegwa.repl.co; 
   );
   next();
-})
+});
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env['CONNECTIONSTRING'], {
+mongoose
+  .connect(process.env["CONNECTIONSTRING"], {
     useNewURLParser: true,
     useUnifiedTopology: true,
-}).then(console.log('MongoDB is now connected.'))
+  })
+  .then(console.log("MongoDB is now connected."));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  console.log("Your app is listening on port " + listener.address().port);
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
-        console.log('Tests are not valid:');
+      } catch (e) {
+        console.log("Tests are not valid:");
         console.error(e);
       }
     }, 3500);
