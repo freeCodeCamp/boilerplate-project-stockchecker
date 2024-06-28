5ef5d03093b5736e8f5ca5e8bd68af3ff5690b99
Gibin Ninan <gibinninan5@gmail.com>
2024-06-28 14:52:23 +0000

    feat: making stock price checker functional

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,81 @@
 'use strict';
+require('isomorphic-fetch')
+require('dotenv').config();
+
+const likes = {};
+
+function getLikes(data = {}) {
+  return Object.values(data).reduce((sum, item) => sum + (item ?? 0), 0);
+}
 
 module.exports = function (app) {
+  const router = app.route('/api/stock-prices');
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  router.get(async function (req, res) {
+    const ipAddr = req.headers['x-forwarded-for'];
+    const { stock, like } = req.query;
+
+    if (!stock) {
+      res.status(400).end();
+    }
+
+    try {
+      let response = {}
+      if (Array.isArray(stock) && stock.length === 2) {
+        const result_1 = await fetch(`${process.env.HOST_STOCK}${stock[0]}/quote`);
+        const result_2 = await fetch(`${process.env.HOST_STOCK}${stock[1]}/quote`);
+
+        const data_1 = await result_1.json();
+        const data_2 = await result_2.json();
+
+        if (like === 'true') {
+          likes[stock[0]] = likes[stock[0]] ? likes[stock[0]] : {};
+          likes[stock[1]] = likes[stock[1]] ? likes[stock[1]] : {};
+          if (!likes[stock[0]][ipAddr] && !likes[stock[1]][ipAddr]) {
+            likes[stock[0]][ipAddr] = 1
+            likes[stock[1]][ipAddr] = 1
+          }
+        }
+
+        response = {
+          stockData: [
+            {
+              stock: data_1.symbol,
+              price: data_1.latestPrice,
+              rel_likes: getLikes(likes[stock[0]]) - getLikes(likes[stock[1]]),
+            },
+            {
+              stock: data_2.symbol,
+              price: data_2.latestPrice,
+              rel_likes: getLikes(likes[stock[1]]) - getLikes(likes[stock[0]]),
+            }
+          ]
+        }
+      } else {
+        const result = await fetch(`${process.env.HOST_STOCK}${stock}/quote`);
+        const data = await result.json();
+
+        if (like === "true") {
+          likes[stock] = likes[stock] ? likes[stock] : {};
+          if (!likes[stock][ipAddr]) {
+            likes[stock][ipAddr] = 1
+          }
+        }
+
+        response = {
+          stockData: {
+            stock: data.symbol,
+            price: data.latestPrice,
+            likes: getLikes(likes[stock]),
+          }
+        }
+      }
+
+      res.status(200).json(response);
+    } catch (err) {
+      console.log(err);
+      res.status(500).end();
+    }
+  });
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,79 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+describe('Functional Tests', function () {
+    it('should return the stock data', done => {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG')
+            .end((err, res) => {
+                assert.isString(res.body.stockData.stock);
+                assert.isNumber(res.body.stockData.price);
+                assert.isNumber(res.body.stockData.likes);
+                assert.strictEqual(res.body.stockData.likes, 0);
+                done();
+            });
+    });
 
+    it('should return the stock data when liked', done => {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG&like=true')
+            .end((err, res) => {
+                assert.isString(res.body.stockData.stock);
+                assert.isNumber(res.body.stockData.price);
+                assert.isNumber(res.body.stockData.likes);
+                assert.strictEqual(res.body.stockData.likes, 1);
+                done();
+            });
+    });
+
+    it('viewing the same stock and liking it again', done => {
+        const requester = chai.request(server);
+
+        requester.get('/api/stock-prices?stock=MSFT&like=true')
+            .end()
+        requester.get('/api/stock-prices?stock=MSFT&like=true')
+            .end((err, res) => {
+                assert.isString(res.body.stockData.stock);
+                assert.isNumber(res.body.stockData.price);
+                assert.isNumber(res.body.stockData.likes);
+                assert.strictEqual(res.body.stockData.likes, 1);
+                done();
+            });
+    });
+
+    it('viewing two stocks', done => {
+        chai.request(server)
+            .get('/api/stock-prices?stock=MSFT&stock=GOOG')
+            .end((err, res) => {
+                assert.isArray(res.body.stockData)
+                assert.isString(res.body.stockData[0].stock);
+                assert.isNumber(res.body.stockData[0].price);
+                assert.isNumber(res.body.stockData[0].rel_likes);
+                assert.strictEqual(res.body.stockData[0].rel_likes, 0);
+
+                assert.isString(res.body.stockData[1].stock);
+                assert.isNumber(res.body.stockData[1].price);
+                assert.isNumber(res.body.stockData[1].rel_likes);
+                assert.strictEqual(res.body.stockData[1].rel_likes, 0);
+                done();
+            });
+    });
+
+    it('viewing two stocks and liking them', done => {
+        chai.request(server)
+            .get('/api/stock-prices?stock=MSFT&stock=GOOG&like=true')
+            .end((err, res) => {
+                assert.isArray(res.body.stockData)
+                assert.isString(res.body.stockData[0].stock);
+                assert.isNumber(res.body.stockData[0].price);
+                assert.isNumber(res.body.stockData[0].rel_likes);
+                assert.strictEqual(res.body.stockData[0].rel_likes, 0);
+
+                assert.isString(res.body.stockData[1].stock);
+                assert.isNumber(res.body.stockData[1].price);
+                assert.isNumber(res.body.stockData[1].rel_likes);
+                assert.strictEqual(res.body.stockData[1].rel_likes, 0);
+                done();
+            });
+    });
 });
